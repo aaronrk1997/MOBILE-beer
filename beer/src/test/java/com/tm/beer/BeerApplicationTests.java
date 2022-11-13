bf15083b7aa4b71fe3821cab2094205f23947bd3
aaron <aaron.rombouts@gmail.com>
2022-11-13 14:40:17 +0100

    initial project init

---
+++ beer/src/test/java/com/tm/beer/BeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tm.beer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
